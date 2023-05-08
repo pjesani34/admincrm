dda074862b9f1044942abdb474713ea7ddfbb3bc
Pratik Jesani <pjesani34@gmail.com>
2023-05-08 19:16:18 +0530

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Feature
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
