5e9c737cd8498163876d627dadbf93f4c901d107
Pratik Jesani <pjesani34@gmail.com>
2023-05-08 21:44:00 +0530

    'registration'

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    #path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .models import Feature
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    #check to see if logged in
+    if request.method =="POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        #authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request, "You Have Been Logged In")
+            return redirect('index')
+        else:
+            messages.success(request, "There was An Error Logging In, Please Try Again!!")
+            return redirect('index')
+    else:
+        return render(request, 'index.html', {})
+
+def logout_user(request):        
+    logout(request)
+    messages.success(request, "You Have Been Logged Out...")
+    return redirect('index')
+
+def register_user(request):
+    return render(request, 'register.html', {})
